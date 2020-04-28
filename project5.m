bb76123e0a515af0e6052fbda3d8fac922a4c477
Jarrett Parker <jarrettparker34@gmail.com>
2020-04-28 17:45:38 -0400

    Added comments for everything we need to do

---
+++ project5.m
@@ -0,0 +1,66 @@
+% Project 5
+% Group 
+% Members: Jarrett Parker, Liam Fitzpatrick, John Anderson
+
+% Gives the input data given to us
+% Name in workspace: InputData
+load('Proj5InputData.mat');
+
+% Encrypt InputData using Cipher Block Chaining
+% Output - Encrpyted Bit Stream (serial)
+% Input: InputData
+% Ouput name in workspace: 
+
+
+% QPSK on the Encrypted Bit Stream
+% Output - Bit to Modulation Symbol Mapping (serial)
+% Input: 
+% Output name in workspace: 
+
+
+% Serial to parallel
+% IFFT
+% Output - OFDM Symbol Generation (parallel)
+% Input: 
+% Output name in workspace: 
+
+
+% Parallel to serial
+% Cyclic prefix insertion
+% Output - CP Insertion (serial)
+% Input: 
+% Output name in workspace: 
+
+
+% Add in channel noise
+% Need to create noise, run through IFFT, add to CP Inseriton output
+% Output - Additive Channel Noise (serial)
+% Input: 
+% Output name in workspace: 
+
+
+% Cyclic prefix removal
+% Output - CP Removal (serial)
+% Input: 
+% Output name in workspace: 
+
+
+% Serial to parallel
+% FFT
+% Parallel to serial
+% Output - Modulation Symbol Recovery (serial)
+% Input: 
+% Output name in workspace: 
+
+
+% Reverse QPSK
+% Output - Modulation Symbol to Encrypted Bits (serial)
+% Input: 
+% Output name in workspace: 
+
+
+% Decrypt Modulation Symbol to Encrypted Bits using Cipher Block Chaining
+% Output - De-encrypted Bit Stream (serial)
+% Input: 
+% Output name in workspace: 
+
